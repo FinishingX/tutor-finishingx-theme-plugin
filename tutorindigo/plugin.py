4922ce02bc497611a88cf9fcd7c0c3f45a9ae35e
Mahendra <MahendraC@TitanEd.com>
2026-02-11 21:52:21 +0530

    Updated initial changes

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -23,8 +23,8 @@ config: t.Dict[str, t.Dict[str, t.Any]] = {
     "defaults": {
         "VERSION": __version__,
         "WELCOME_MESSAGE": "The place for all your online learning",
-        "PRIMARY_COLOR": "#15376D",  # Indigo
-        "ENABLE_DARK_TOGGLE": True,
+        "PRIMARY_COLOR": "#6EACAF",  # Indigo
+        "ENABLE_DARK_TOGGLE": False,
         # Footer links are dictionaries with a "title" and "url"
         # To remove all links, run:
         # tutor config save --set INDIGO_FOOTER_NAV_LINKS=[]
@@ -113,6 +113,9 @@ indigo_styled_mfes = [
     "profile",
     "account",
     "discussions",
+    "communications",
+    "gradebook",
+    "ora-grading"
 ]
 
 for mfe in indigo_styled_mfes:
@@ -121,7 +124,7 @@ for mfe in indigo_styled_mfes:
             (
                 f"mfe-dockerfile-post-npm-install-{mfe}",
                 """
-RUN npm install '@edx/brand@github:@edly-io/brand-openedx#indigo-2.5.0'
+RUN npm install '@edx/brand@git+https://github.com/FinishingX/finishingx-brand.git'
 """,  # noqa: E501
             ),
         ]
@@ -130,7 +133,13 @@ RUN npm install '@edx/brand@github:@edly-io/brand-openedx#indigo-2.5.0'
 hooks.Filters.ENV_PATCHES.add_item(
     (
         "mfe-dockerfile-post-npm-install-authn",
-        "RUN npm install '@edx/brand@github:@edly-io/brand-openedx#indigo-2.5.0'",
+        "RUN npm install '@edx/brand@git+https://github.com/FinishingX/finishingx-brand.git'",
+    )
+)
+hooks.Filters.ENV_PATCHES.add_item(
+    (
+        "mfe-dockerfile-post-npm-install-authoring",
+        "RUN npm install '@edx/brand@git+https://github.com/FinishingX/finishingx-brand.git'",
     )
 )
 
